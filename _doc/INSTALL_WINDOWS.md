facf090709791b3b40fdec098801f6e71340faaa
Matthias <matthes@leuffen.de>
2017-07-23 01:08:36 +0200

    Changed Windows-Boot-Script

---
+++ _doc/INSTALL_WINDOWS.md
@@ -0,0 +1,11 @@
+# How to start on Windows
+
+## Windows 10 Home Edition
+
+- Download and install Docker-Toolbox from [docker.com](https://www.docker.com/products/docker-toolbox)
+
+- Open your favorite Command-Promt (e.g. Powershell or cmd)
+
+- Type in: ``docker-machine env | Invoke-Expression``
+
+
--- golafix-developer.bat
+++ golafix-developer.bat
@@ -1,4 +1,14 @@
 
+@ECHO OFF
 
+SET _curPath=%cd%
+SET _lnPath=%_curPath:\=/%
+SET _lnPath=%_lnPath::=%
 
-docker run -p 80:80 -v "$PWD:/opt" -it golafix/developer
+## Small Device character
+SET _lnPath=%_lnPath:C=c%
+SET _lnPath=%_lnPath:D=d%
+SET _lnPath=%_lnPath:E=e%
+@ECHO ON
+
+docker run -p 80:80 -v ///%_lnPath%:/opt -it golafix/developer
